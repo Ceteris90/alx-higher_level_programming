083da38ce2688013fff76ae90368d665673ef7d0
Ceteris90 <jonas.nyador@mathmods.eu>
2023-03-10 15:03:29 -0800

    Python Loop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import sys
+
     argv = sys.argv[1:]
     argv_count = len(argv)
     index = 1
+
     if argv_count is 0:
         print("{:d} arguments.".format(argv_count))
     elif argv_count is 1:
