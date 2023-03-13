b82d7f5746dda20320766616ccf6d44790feaefd
Ceteris90 <jonas.nyador@mathmods.eu>
2023-03-13 03:44:33 -0700

    Python Loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
+
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    len_my_list = len(my_list)
     if idx < 0:
         return None
-    if idx => len_my_list:
+    if idx >= len(my_list):
         return None
     return my_list[idx]
