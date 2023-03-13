f3655eb8691815bb0be0a21b5a0f83673e88406b
Ceteris90 <jonas.nyador@mathmods.eu>
2023-03-13 04:02:48 -0700

    Python Loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    len_my_list = len(my_list)
     if idx < 0:
         return my_list
-    if idx => len_my_list:
+    if idx >= len(my_list):
         return my_list
     my_list[idx] = element
     return my_list
