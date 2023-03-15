63af904aa114f69daceb82a984dec3b73e947e0b
Ceteris90 <jonas.nyador@mathmods.eu>
2023-03-14 23:36:22 -0700

    Python Data

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,14 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    biggest = 0
+    biggest = []
     for index in my_list:
-        if index > biggest:
-            biggest = index
-        
-        negative = []
-        if index < bigest:
-            negative.append(index)
-            bigest = max(negative)
+        biggest.append(index)
 
-    return biggest
+    return max(biggest)
