a53fee03a9a003e023fe7b8da7224ee716b2cc32
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-06 00:26:26 -0700

    Everything

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,1 +1,12 @@
-[1, 2, 3]
+#!/usr/bin/python3
+"""
+copy a list
+"""
+
+
+def copy_list(l):
+    """generates a copy of a list
+    """
+    if not isinstance(l, list):
+        return None
+    return l[:]
