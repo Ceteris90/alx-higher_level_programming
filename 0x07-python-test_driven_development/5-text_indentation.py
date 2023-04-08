d02241738c761f9c23f5af1ecf59cb0e90c9e655
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-08 05:54:23 -0700

    test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+The text_indentation module for adding newlines after certain characters in a text.
+"""
+
+def text_indentation(text):
+    """ Adds two newlines """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
