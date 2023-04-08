a00667521a76702667bd0407a54a01d370cf88ac
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-08 05:32:10 -0700

    test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+The say_my_name module for printing a person's first and last name.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints a string with <first_name> and <last_name>."""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
