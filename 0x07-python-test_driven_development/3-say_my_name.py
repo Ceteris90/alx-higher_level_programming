e6cb742928c0a2bb08155f5036a7114bb556be11
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-08 05:38:30 -0700

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 The say_my_name module for printing a person's first and last name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """Prints a string with <first_name> and <last_name>."""
 
