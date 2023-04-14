2b21a8f94cf340224242c57c7a8a724d8a47d3fa
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-13 17:16:19 -0700

    input/output

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creates a class.
+"""
+
+
+class BaseGeometry:
+    """Class with public instance"""
+
+    def area(self):
+        """Exception message
+        'area() is not implemented'.
+        """
+
+        raise Exception('area() is not implemented')
