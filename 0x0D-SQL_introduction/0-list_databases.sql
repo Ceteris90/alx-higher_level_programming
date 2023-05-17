dc7c437366459de5a82127f112ca8559aef06f01
Ceteris90 <jonas.nyador@mathmods.eu>
2023-05-17 06:34:55 -0700

    SQL

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,26 +1,2 @@
-#!/usr/bin/python3
-import mysql.connector
-
-# Establish a connection to the MySQL server
-cnx = mysql.connector.connect(
-    host='localhost',
-    user='your_username',
-    password='your_password'
-)
-
-# Create a cursor object to execute SQL queries
-cursor = cnx.cursor()
-
-# Execute the query to retrieve all databases
-cursor.execute("SHOW DATABASES")
-
-# Fetch all the database names from the result set
-database_names = [row[0] for row in cursor]
-
-# Print the list of databases
-for database_name in database_names:
-    print(database_name)
-
-# Close the cursor and connection
-cursor.close()
-cnx.close()
+--This script to list all the databases in MySQL in the server
+SHOW DATABASES;
